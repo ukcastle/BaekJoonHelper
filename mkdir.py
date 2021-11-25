2d598c00ee16ff1bb1f53e3674d6bcd5b09913cf
Sangwon <zzxng123@gmail.com>
2021-11-25 20:59:18 +0900

    Modify mkdir

--- mkdir.py
+++ mkdir.py
@@ -1,4 +1,4 @@
-from src.methods import getNamespaceAndArgs, getTitleByNumber
+from src.methods import getNamespaceAndArgs, getTitleByNumber, getProblem
 import os
 import shutil
 
@@ -7,13 +7,51 @@ if __name__ == '__main__':
 	
 	try:
 		name = getTitleByNumber(ns.number)
+		problem_description, input_description, output_description, sample_i, sample_o = getProblem(ns.number)
 	except:
 		print(f"{ns.number}번 문제를 찾을 수 없습니다")
 		exit(0)
 
-	os.mkdir(f"./{name}")
+	os.mkdir(f"./Problem/{name}")
 	
-	shutil.copy("./src/solution.py", f"./{name}/")
-	shutil.copy("./src/test_case.py", f"./{name}/")
+	shutil.copy("./src/solution.py", f"./Problem/{name}/")
+	# shutil.copy("./src/test_case.py", f"./Problem/{name}/")
+	
+	inputs = []
+	outputs = []
+	
+	# readme.md 파일 생성
+	f = open(f"./Problem/{name}/README.md", 'w', encoding='utf-8')
+	f.write('### 문제\n')
+	f.write('\n'.join([str(i).replace("src=\"", "src=\"https://www.acmicpc.net/") for i in problem_description]))
+	f.write('\n<hr/>\n\n')
+	f.write('### 입력\n')
+	f.write('\n'.join([str(i) for i in input_description]))
+	f.write('\n<hr/>\n\n')
+	f.write('### 출력\n')
+	f.write('\n'.join([str(i) for i in output_description]))
+	f.write('\n<hr/>\n\n')
+	for i in range(len(sample_i)):
+		f.write('### 입력 예시 {}\n'.format(i+1))
+		f.write(sample_i[i].text + '\n')
+		inputs.append(sample_i[i].text)
+		f.write('### 출력 예시 {}\n'.format(i+1))
+		f.write(sample_o[i].text + '\n<hr/>\n\n')
+		outputs.append(sample_o[i].text)
+	f.close()
+	
+	# test_case 수정
+	f = open("./src/test_case.py", 'r', encoding='utf-8')
+	py_file = f.read()
+	f.close()
+
+	# 수정
+	py_file = py_file.replace("\"input_modify\"", str(inputs))
+	py_file = py_file.replace("\"output_modify\"", str(outputs))
+	
+	# 작성
+	f = open(f"./Problem/{name}/test_case.py", 'w', encoding='utf-8')
+	f.write(py_file)
+	f.close()
 
 	print("url : ",f"https://www.acmicpc.net/problem/{ns.number}")
--- src/methods.py
+++ src/methods.py
@@ -6,10 +6,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 def getFileName(questionNum):
-	folderName = [x for x in os.listdir() if re.findall(rf"^{questionNum}_",x)]
+	folderName = [x for x in os.listdir('./Problem') if re.findall(rf"^{questionNum}_",x)]
 
 	if len(folderName) == 1:
-		return folderName[0]
+		return 'Problem.' + folderName[0]
 	
 	for name in folderName:
 		print(f"검색된 폴더 이름 : {name}")
@@ -44,3 +44,17 @@ def getTitleByNumber(number):
 	html = urlopen(f"https://www.acmicpc.net/problem/{number}")
 	bsObject = BeautifulSoup(html, "html.parser")
 	return bsObject.head.findAll("title")[0].__str__()[7:-8].replace("번: ","_")
+
+def getProblem(number):
+	html = urlopen(f"https://www.acmicpc.net/problem/{number}")
+	bsObject = BeautifulSoup(html, "html.parser")
+
+	# 문제 가져오기
+	problem_description = bsObject.select('#problem_description > p')
+	# problem_ul = bsObject.select('#problem_description > ul')
+	input_description = bsObject.select('#problem_input > p')
+	output_description = bsObject.select('#problem_output > p')
+	sample_i = bsObject.select("pre[id^=sample-input]")
+	sample_o = bsObject.select("pre[id^=sample-output]")
+
+	return problem_description, input_description, output_description, sample_i, sample_o
